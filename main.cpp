d56e80333ea8501cfee25a7de50bc9ba66c2391f
Jakub Ogrodnik <81594386+JakubOgrodnikPL@users.noreply.github.com>
2022-07-12 18:14:44 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream> //Include library iostream input-output stream.
+
+using namespace std; // using standard namespace
+
+int main() //Main function of the program; int = integer at the end it returns integer value
+{
+    cout << "Hello world! \n" << endl; // instruction cout to print
+    cout << "Press any key to exit" << endl;
+
+    getchar(); //Program is waiting for character from keyboard holding the window on the screen
+
+    return 0;
+}
